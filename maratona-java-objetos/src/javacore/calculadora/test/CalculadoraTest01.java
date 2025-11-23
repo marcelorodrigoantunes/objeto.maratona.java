e914b8b58e0f70f38bd4241c91806b393903487a
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-23 10:43:58 -0300

    Calculadora

---
+++ maratona-java-objetos/src/javacore/calculadora/test/CalculadoraTest01.java
@@ -0,0 +1,12 @@
+package javacore.calculadora.test;
+
+import javacore.calculadora.metodos.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01");
+        calculadora.subtraiDoisNumeros();
+    }
+}
---
+++ maratona-java-objetos/src/javacore/calculadora/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package javacore.calculadora.test;
+
+import javacore.calculadora.metodos.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10,20);
+    }
+}
