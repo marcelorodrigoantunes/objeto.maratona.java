ca86533f3b3d01f48da7f83476f3dcff3da6fe36
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-25 09:27:48 -0300

    impressora

---
+++ maratona-java-objetos/src/javacore/estudantedois/test/EstudanteTest01.java
@@ -0,0 +1,23 @@
+package javacore.estudantedois.test;
+
+import javacore.estudantedois.dominio.Estudante;
+import javacore.estudantedois.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante01.nome = "Marcelo";
+        estudante01.idade = 15;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Rodrigo";
+        estudante02.idade = 16;
+        estudante02.sexo = 'M';
+
+        impressora.imprime(estudante01);
+        impressora.imprime(estudante02);
+    }
+}
