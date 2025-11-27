9de4344f429c5e97763d0b98c49777bea3a66ca3
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-27 11:57:16 -0300

    get

--- maratona-java-objetos/src/javacore/funcionario/test/FuncionarioTest01.java
+++ maratona-java-objetos/src/javacore/funcionario/test/FuncionarioTest01.java
@@ -5,10 +5,9 @@ import javacore.funcionario.Funcionario;
 public class FuncionarioTest01 {
     public static void main(String[] args){
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Marcelo";
-        funcionario.idade = 23;
-        funcionario.salarios = new double[]{120, 987.32, 2000};
-
+        funcionario.setNome("Marcelo");
+        funcionario.setIdade(23);
+        funcionario.setSalarios(new double[]{1200, 987.32, 2000});
         funcionario.imprime();
     }
 }
