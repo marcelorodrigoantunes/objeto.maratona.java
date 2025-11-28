ff9def4fc35d1206c1a62654685f4a9ac5c0b180
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-28 11:27:25 -0300

    Anime

---
+++ maratona-java-objetos/src/javacore/anime/test/AnimeTest01.java
@@ -0,0 +1,16 @@
+package javacore.anime.test;
+
+import javacore.anime.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        //anime.init("Akudama Drive","TV",12);
+        anime.init("Akudama Drive","TV",12, "Ação");
+        //anime.setGenero("Ação");
+        //anime.setNome("Akudama Drive");
+        //anime.setTipo("TV");
+        //anime.setEpisodios(12);
+        anime.imprime();
+    }
+}
