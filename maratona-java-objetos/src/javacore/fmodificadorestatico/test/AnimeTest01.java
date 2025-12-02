2852926381263c5752eeca3e96bab904958ed936
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-12-02 11:39:28 -0300

    Bloco inicialização

---
+++ maratona-java-objetos/src/javacore/fmodificadorestatico/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package javacore.fmodificadorestatico.test;
+
+import javacore.fmodificadorestatico.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
