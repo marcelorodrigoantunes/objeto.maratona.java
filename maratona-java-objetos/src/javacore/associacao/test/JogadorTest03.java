63d165d918eca59202fa2debae68f8cc13ad0d15
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-12-06 11:13:07 -0300

    Time

---
+++ maratona-java-objetos/src/javacore/associacao/test/JogadorTest03.java
@@ -0,0 +1,22 @@
+package javacore.associacao.test;
+
+import javacore.associacao.dominio.Jogador;
+import javacore.associacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pele");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador, jogador2};
+
+        jogador.setTime(time);
+        jogador2.setTime(time);
+        time.setJogadores(jogadores);
+
+        System.out.println("--------Jogador---------");
+        jogador.imprime();
+        System.out.println("--------Time---------");
+        time.imprime();
+    }
+}
