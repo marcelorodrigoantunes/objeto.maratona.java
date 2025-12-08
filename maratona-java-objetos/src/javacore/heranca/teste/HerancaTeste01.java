3caee8c02af9301920b0868e0e6aed773c01b8de
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-12-08 12:04:27 -0300

    Herança

---
+++ maratona-java-objetos/src/javacore/heranca/teste/HerancaTeste01.java
@@ -0,0 +1,27 @@
+package javacore.heranca.teste;
+
+import javacore.heranca.dominio.Endereco;
+import javacore.heranca.dominio.Funcionario;
+import javacore.heranca.dominio.Pessoa;
+
+public class HerancaTeste01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3 ");
+        endereco.setCep("12312-234");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Marcelo Antunes");
+        pessoa.setCpf("11111111");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("João");
+        funcionario.setCpf("22222222");
+        funcionario.setEndereco(endereco);
+        System.out.println("------------");
+        funcionario.setSalario(2000);
+
+        funcionario.imprime();
+    }
+}
