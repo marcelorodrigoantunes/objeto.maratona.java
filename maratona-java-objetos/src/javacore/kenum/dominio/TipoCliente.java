6faa1f3cc97a3439028cc6f56ed860a717531589
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-12-10 11:08:03 -0300

    Enumeração

--- maratona-java-objetos/src/javacore/kenum/dominio/TipoCliente.java
+++ maratona-java-objetos/src/javacore/kenum/dominio/TipoCliente.java
@@ -1,6 +1,30 @@
 package javacore.kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Fisica"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
+
+    private int valor;
+    private String nomeRelatorio;
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.valor = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatorio){
+        for (TipoCliente tipoCliente : values()){
+            if (tipoCliente.getNomeRelatorio().equals(nomeRelatorio)){
+                return tipoCliente;
+            }
+        }
+        return null;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
+
+    public int getValor() {
+        return valor;
+    }
 }
--- maratona-java-objetos/src/javacore/kenum/teste/ClienteTeste01.java
+++ maratona-java-objetos/src/javacore/kenum/teste/ClienteTeste01.java
@@ -2,17 +2,20 @@ package javacore.kenum.teste;
 
 import javacore.kenum.dominio.Cliente;
 import javacore.kenum.dominio.TipoCliente;
+import javacore.kenum.dominio.TipoPagamento;
 
 public class ClienteTeste01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Marcelo", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Joao",TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Jose",TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Pedro",TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Marcelo", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Pedro",TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente.getNomeRelatorio());
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Fisica");
+        System.out.println(tipoCliente2);
     }
 }
